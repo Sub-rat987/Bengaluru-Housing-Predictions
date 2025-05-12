a97c7441e4b675a80f1c51f626d051e9f4eecf68
Sub-rat987 <subratonization@gmail.com>
2025-05-12 10:45:56 +0530

    port binding changes for render

--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 import util
+import os
+
+
 
 app = Flask(__name__)
 
@@ -29,4 +32,5 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
